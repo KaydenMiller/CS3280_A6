78ad164ebca6e19a5596e5d5a761ab69bae48ee9
Kayden Miller <kaydenmiller1@gmail.com>
2018-10-25 22:48:37 -0600

    Add a seat custom control. Add a passenger POCO. Add a passengerViewModel DTO. Add a CreatePassengerWindow.

---
+++ CS3280_Assignment6/Models/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3280_Assignment6.Models
+{
+    public class Passenger
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
